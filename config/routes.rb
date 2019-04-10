bbb7a2b7de0d667e228837e204cfde7346c488f5
siberry <jwy5140@gmail.com>
2019-04-10 13:53:37 -0400

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'author/index'
+  get 'author/show'
+  get 'author/new'
+  get 'author/edit'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
